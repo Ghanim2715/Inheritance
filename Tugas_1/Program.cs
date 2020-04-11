72ebcda8834e54a2c9a2327c9347dda62bd09494
Ghanim <muhammad.2715@students.amikom.ac.id>
2020-04-11 16:58:16 +0700

    inisialisasiproject

---
+++ Tugas_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Eve",18);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Triton",35,190302,"Math","Dosen@teacher.amikom.ac.id");
+            teacher.GetNameAndAge();
+            
+            Student student = new Student("Ghanim",18,2715,"muhammad.2715@student.amikom.ac.id");
+            student.GetNameAndAge();
+        }
+    }
+}
